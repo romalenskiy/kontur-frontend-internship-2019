1dccc42c72f13354b9cd63b8e1dbfea33196576e
romalenskiy <kuperman88@gmail.com>
2019-03-12 20:34:02 +0300

    File structure refactored / code formatted

--- tests/sort/tests.js
+++ tests/sort/tests.js
@@ -1,23 +1,23 @@
-const chai = require('chai');
-const path = require('path');
-const child = require('child_process');
+const chai = require('chai')
+const path = require('path')
+const child = require('child_process')
 
-const { expect } = chai;
-let proc;
-const exec = path.join(__dirname, '../..', 'index.js');
+const { expect } = chai
+let proc
+const exec = path.join(__dirname, '../../src', 'index.js')
 
 
 describe('Команда sort', () => {
-    before(() => {
-        process.chdir(__dirname);
-    });
+  before(() => {
+    process.chdir(__dirname)
+  })
 
-    beforeEach(() => {
-        proc = child.exec('node ' + exec);
-    });
+  beforeEach(() => {
+    proc = child.exec(`node ${exec}`)
+  })
 
-    it('должен показывать список todo отсортированный по важности', (done) => {
-        const result = `
+  it('должен показывать список todo отсортированный по важности', (done) => {
+    const result = `
   !  |  user      |  date        |  comment                              |  fileName       
 -------------------------------------------------------------------------------------------
   !  |  pe        |  2018-12-26  |  Работать пора!!!                     |  jsWithTodo.js  
@@ -31,20 +31,20 @@ describe('Команда sort', () => {
      |            |              |  Как дела?                            |  jsWithTodo.js  
      |            |              |  Не понимаю, что здесь происходит...  |  jsWithTodo.js  
 -------------------------------------------------------------------------------------------
-`.trim();
+`.trim()
 
-        proc.stdout.once('data', function(){
-            proc.stdin.write('sort importance\r');
-            proc.stdout.once('data', function(output){
-                expect(output.toString('utf-8').trim()).to.eq(result);
-                done();
-            });
-        });
-    });
+    proc.stdout.once('data', () => {
+      proc.stdin.write('sort importance\r')
+      proc.stdout.once('data', (output) => {
+        expect(output.toString('utf-8').trim()).to.eq(result)
+        done()
+      })
+    })
+  })
 
 
-    it('должен показывать список todo отсортированный по дате', (done) => {
-        const result = `
+  it('должен показывать список todo отсортированный по дате', (done) => {
+    const result = `
   !  |  user      |  date        |  comment                              |  fileName       
 -------------------------------------------------------------------------------------------
   !  |  pe        |  2018-12-26  |  Работать пора!!!                     |  jsWithTodo.js  
@@ -58,20 +58,20 @@ describe('Команда sort', () => {
      |            |              |  Как дела?                            |  jsWithTodo.js  
      |            |              |  Не понимаю, что здесь происходит...  |  jsWithTodo.js  
 -------------------------------------------------------------------------------------------
-`.trim();
+`.trim()
 
-        proc.stdout.once('data', function(){
-            proc.stdin.write('sort date\r');
-            proc.stdout.once('data', function(output){
-                expect(output.toString('utf-8').trim()).to.eq(result);
-                done();
-            });
-        });
-    });
+    proc.stdout.once('data', () => {
+      proc.stdin.write('sort date\r')
+      proc.stdout.once('data', (output) => {
+        expect(output.toString('utf-8').trim()).to.eq(result)
+        done()
+      })
+    })
+  })
 
 
-    it('должен показывать список todo отсортированный по пользователям', (done) => {
-        const result = `
+  it('должен показывать список todo отсортированный по пользователям', (done) => {
+    const result = `
   !  |  user      |  date        |  comment                              |  fileName       
 -------------------------------------------------------------------------------------------
   !  |  pe        |  2018-12-26  |  Работать пора!!!                     |  jsWithTodo.js  
@@ -85,20 +85,14 @@ describe('Команда sort', () => {
      |            |              |  Как дела?                            |  jsWithTodo.js  
      |            |              |  Не понимаю, что здесь происходит...  |  jsWithTodo.js  
 -------------------------------------------------------------------------------------------
-`.trim();
+`.trim()
 
-        proc.stdout.once('data', function(){
-            proc.stdin.write('sort user\r');
-            proc.stdout.once('data', function(output){
-                expect(output.toString('utf-8').trim()).to.eq(result);
-                done();
-            });
-        });
-    });
-});
-
-
-
-
-
-
+    proc.stdout.once('data', () => {
+      proc.stdin.write('sort user\r')
+      proc.stdout.once('data', (output) => {
+        expect(output.toString('utf-8').trim()).to.eq(result)
+        done()
+      })
+    })
+  })
+})
