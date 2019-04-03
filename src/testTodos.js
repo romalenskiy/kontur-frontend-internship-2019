6fad5d801e32177372765f3c4a88a14ae1d952a1
romalenskiy <kuperman88@gmail.com>
2019-04-03 18:58:04 +0300

    gitignore edited

---
+++ src/testTodos.js
@@ -0,0 +1,44 @@
+// Todo : Veronika; 2018; Sample Text
+// TODO : VeRoNiKa69; 2018; Sample Text
+// Todo : VERONIKA; 2018; Sample Text
+// Todo : ve                                 ;           2018          ; Sample Text
+// Todo : Velka; 2018; Sample Text
+// Todo : Mrs.Veronika; 2018-08; Sample Text
+// todo : .seekTripper;;Do some cool stuff!!;dsad;sadasd;asd
+// todo: foo;
+// todo:bar;
+//todo
+const a = '' //TODO test1
+// todo test2
+//	todo test3
+// 	todo test4
+//	 todo test5
+//	 	todo test6
+// 	 tODO test7
+//                                             todO test8
+//																			
+//					todo test9
+//  	 	 	 		     	 	 	 todo test10
+
+//
+todo test 11
+
+// todo foobar
+// todo foo;bar
+// todo ;
+
+// todo :
+// todo : ::::::::::::::::::::::::::::::
+// todo : :foo
+
+//todo Andy; 2019-03-32; hi
+// TODO Helga; 2019-30; how are you? 
+// todo Andrew ; 2019/03/30; fine, thx!
+// todo Man; 2019.03.30.; pull
+// todo Mel; 2019-03-30-; lul
+//todo Ana; 2019-03-; NaN
+// todo Paul; 2019_03-21; dafaq
+
+// todo BOB; 2019-1-2; I am from village!
+
+// todo user; asd;sad
